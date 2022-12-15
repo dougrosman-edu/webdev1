93a0b5d163e1076381fe9b688eb0a5b5ed72941e
dougrosman-edu <drosma@artic.edu>
2022-12-15 11:32:15 -0600

    remove stuff

--- projects/geocities/main.js
+++ projects/geocities/main.js
@@ -1,7 +1,11 @@
-let dancefloors = document.querySelectorAll(".dancefloor");
+const NUM_DANCERS = 98;
 
-const NUM_DANCERS = 97;
-
-for(let i = 1; i < NUM_DANCERS; i++) {
-   
+for(let i = 0; i < NUM_DANCERS; i++) {
+   const dancer = document.createElement("img");
+   const gifSrc = `gifs/dancing/${i}.gif`;
+   dancer.setAttribute("src", gifSrc);
+   dancer.setAttribute("alt", `dancing gif #${i}`);
+   dancer.classList.add("dancer");
+   dancer.style.left = `${(Math.random() * 85)}%`
+   danceFloor.append(dancer);
 }
