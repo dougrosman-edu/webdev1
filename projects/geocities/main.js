94d55ebabeb2138c483c6f7a09f6c0e6456ef108
dougrosman-edu <drosma@artic.edu>
2022-12-15 12:29:20 -0600

    let's dance

--- projects/geocities/main.js
+++ projects/geocities/main.js
@@ -1,4 +1,4 @@
-const NUM_DANCERS = 98;
+const NUM_DANCERS = 87;
 
 for(let i = 0; i < NUM_DANCERS; i++) {
    const dancer = document.createElement("img");
@@ -6,6 +6,11 @@ for(let i = 0; i < NUM_DANCERS; i++) {
    dancer.setAttribute("src", gifSrc);
    dancer.setAttribute("alt", `dancing gif #${i}`);
    dancer.classList.add("dancer");
-   dancer.style.left = `${(Math.random() * 85)}%`
+   dancer.style.left = `${(Math.random() * 90)}%`;
+   const randomZIndex = (Math.random() * NUM_DANCERS)
+   dancer.style.zIndex = `${randomZIndex}`;
+   const randomYPos = (Math.random() * 50) + 100;
+   console.log(randomYPos)
+   dancer.style.transform = `scale(1.25) translateY(-${randomYPos}%)`;
    danceFloor.append(dancer);
 }
