5413411690144dd5fbbb90441448166dca608217
dougrosman-edu <drosma@artic.edu>
2022-12-15 18:03:20 -0600

    ok

--- projects/geocities/main.js
+++ projects/geocities/main.js
@@ -10,7 +10,6 @@ for(let i = 0; i < NUM_DANCERS; i++) {
    const randomZIndex = Math.floor((Math.random() * NUM_DANCERS));
    dancer.style.zIndex = `${randomZIndex}`;
    const randomYPos = (Math.random() * 50) + 100;
-   console.log(randomYPos)
    dancer.style.transform = `scale(1.25) translateY(-${randomYPos}%)`;
    danceFloor.append(dancer);
 }
